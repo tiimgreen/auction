f3873143be614bdc2aadbe9299d91d9b4f741fbb
Tim Green <tiimgreen@gmail.com>
2015-08-12 00:00:50 +0100

    Finish off app

--- lib/place_bid.rb
+++ lib/place_bid.rb
@@ -1,15 +1,20 @@
 class PlaceBid
-  attr_accessor :error
-  attr_reader :auction
+  attr_reader :error, :auction, :status
 
   def initialize(options)
     @value = options[:value].to_f
-    @user_id = options[:user_id]
+    @user_id = options[:user_id].to_i
     @auction_id = options[:product_auction_id]
   end
 
   def execute
     @auction = ProductAuction.find(@auction_id)
+
+    if @auction.ended? && @auction.top_bid.user_id == @user_id
+      @status = :won
+      @error = 'The auction has ended'
+      return false
+    end
 
     if @value <= @auction.current_bid
       @error = 'That bid is below the current bid'
--- spec/lib/place_bid_spec.rb
+++ spec/lib/place_bid_spec.rb
@@ -4,9 +4,14 @@ require 'place_bid'
 describe PlaceBid do
   let(:selling_user) { FactoryGirl.create(:user) }
   let(:bidding_user) { FactoryGirl.create(:user) }
+  let(:another_user) { FactoryGirl.create(:user) }
   let(:product) { FactoryGirl.create(:product, user_id: selling_user.id) }
   let(:product_auction) do
-    FactoryGirl.create(:product_auction, product_id: product.id, value: 10)
+    FactoryGirl.create(:product_auction,
+      product_id: product.id,
+      value: 10,
+      ends_at: Time.now + 24.hours
+    )
   end
 
   it 'places a bid' do
@@ -31,5 +36,28 @@ describe PlaceBid do
 
     expect(product_auction.current_bid).to eq(10)
     expect(service.error).to eq('That bid is below the current bid')
+  end
+
+  it 'notifies user the auction has ended' do
+    service = PlaceBid.new(
+      value: 11,
+      user_id: bidding_user.id,
+      product_auction_id: product_auction.id
+    )
+
+    service.execute
+
+    another_service = PlaceBid.new(
+      value: 12,
+      user_id: bidding_user.id,
+      product_auction_id: product_auction.id
+    )
+
+    Timecop.travel(Time.now + 25.hours)
+    another_service.execute
+
+    expect(product_auction.current_bid).to eq(11)
+    expect(another_service.status).to eq(:won)
+    expect(another_service.error).to eq('The auction has ended')
   end
 end
